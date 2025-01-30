c8dcd04cc622eb68f22bba5e5cf1522a4d56d487
w311 <wjas.ma@gmail.com>
2025-01-29 23:47:08 -0300

    improve UI with some helpers

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,36 @@
+require 'dotiw'
+include ActionView::Helpers::DateHelper
+include ActionView::Helpers::TextHelper
+include ActionView::Helpers::NumberHelper
+
 class Task < ApplicationRecord
   validates :name, :description, presence: true
   
-  STATUSES = %w[in_progress complete archived].freeze
+  STATUSES = %w[in_progress completed archived].freeze
+
+  def formated_deadline
+    case deadline
+    when 'No deadline.', 'Today'  then 'text-gray-300'
+    when 'Expired'                then 'text-rose-500 animate-pulse'
+    else 'text-gray-300'
+    end
+  end
 
   def completed?
     status == 'completed'
+  end
+
+  def deadline    
+    return 'No deadline.' if expiration_date.nil?
+
+    if expiration_date == Date.today 
+      'Today'
+    else
+      expiration_date.past? ? 'Expired!' : distance_of_time_in_words(Date.today, expiration_date)
+    end
+  end
+
+  def expired?
+    !expiration_date.nil? && expiration_date != Date.today && expiration_date.past?
   end
 end
