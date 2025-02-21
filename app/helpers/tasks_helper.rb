159ee43d263868b277cf93b91368fb29f9059511
w311 <wjas.ma@gmail.com>
2025-02-21 01:54:53 -0300

    implmented badges in task status

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,17 +1,26 @@
 module TasksHelper
   def icon_by_status(status)
-    if status == 'in_progress'
-      'calendar-clock' 
+    if status == "in_progress"
+      "calendar-clock"
     else
-      'calendar-check-2'
+      "calendar-check-2"
     end
   end
 
   def detail_icon(status)
-    if status == 'in_progress'
-      'list-checks' 
+    if status == "in_progress"
+      "list-checks"
     else
-      'square-check'
+      "square-check"
+    end
+  end
+
+  def badge_style_by_status(deadline)
+    case deadline
+    when "Today"        then "badge_default"
+    when "Expired"      then "badge_red"
+    when "No deadline"  then "badge_neutral"
+    else "badge_green"
     end
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,36 +1,30 @@
-require 'dotiw'
+require "dotiw"
 include ActionView::Helpers::DateHelper
-#include ActionView::Helpers::TextHelper
-#include ActionView::Helpers::NumberHelper
 
 class Task < ApplicationRecord
   validates :name, :description, presence: true
-  
+
   STATUSES = %w[in_progress completed archived].freeze
 
-  def a_day
-    expiration_date < Date.today ? 'Expirada' : 'No prazo'
-  end
-
   def in_progress?
-    status == 'in_progress'
+    status == "in_progress"
   end
 
   def completed?
-    status == 'completed'
+    status == "completed"
   end
 
   def archived?
-    status == 'archived'
+    status == "archived"
   end
 
-  def deadline    
-    return 'No deadline' if expiration_date.nil?
+  def deadline
+    return "No deadline" if expiration_date.nil?
 
     if expiration_date == Date.today
-      'Today'
+      "Today"
     else
-      expired? ? 'Expired' : distance_of_time_in_words(Date.today, expiration_date)
+      expired? ? "Expired" : "In progress"
     end
   end
 
