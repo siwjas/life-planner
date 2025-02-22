806bb5ca6dbb2e6e5ec528eb8c004e6bbdd107f2
w311 <wjas.ma@gmail.com>
2025-02-22 04:52:29 -0300

    some style adjustments for contrast improvement

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,19 +9,19 @@ module ApplicationHelper
 
   def nav_class(path)
     if current_page?(path)
-      "shadow py-3 px-5 inline-flex items-center bg-emerald-400 dark:bg-gray-800 text-gray-100
+      "shadow py-3 px-5 inline-flex items-center bg-stone-500 dark:bg-gray-800 text-gray-100
       hover:shadow-md text-sm rounded-lg font-medium transition duration-300 ease-in-out"
     else
-      "shadow py-3 px-5 inline-flex items-center bg-gray-100 hover:bg-emerald-400 dark:bg-transparent dark:hover:bg-gray-800
+      "shadow text-gray-100 py-3 px-5 inline-flex items-center bg-stone-400 hover:bg-stone-500 dark:bg-transparent dark:hover:bg-gray-800
       hover:text-white hover:shadow-md text-sm rounded-lg font-medium transition duration-300 ease-in-out"
     end
   end
 
   def nav_class_mobile(path)
     if current_page?(path)
-      "block rounded-md bg-emerald-400 px-3 py-3 text-base font-medium text-gray-800"
+      "block rounded-md bg-stone-600 px-3 py-3 text-base font-medium text-gray-100"
     else
-      "block rounded-md px-3 py-3 text-base font-medium text-gray-800 hover:bg-gray-600 hover:text-gray-100
+      "block rounded-md px-3 py-3 text-base font-medium text-gray-800 hover:bg-stone-500 hover:text-gray-100
        dark:text-gray-300 dark:hover:text-gray-100 transition duration-300 ease-in-out"
     end
   end
