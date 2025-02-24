afb661252b8a6f21c356459444393498bb8ef5ed
w311 <wjas.ma@gmail.com>
2025-02-23 22:15:24 -0300

    style adjusts and implements task count on view

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -7,29 +7,11 @@ module TasksHelper
     end
   end
 
-
-
   def detail_icon(status)
     if status == "in_progress"
       "list-checks"
     else
       "square-check"
     end
-  end
-
-  def badge_style_by_status(deadline)
-    [
-      { "badge_greent": deadline == "In progress" },
-      { "badge_defalt": deadline == "Today" },
-      { "badge_red": deadline == "Expired" },
-      { "badge_neutral": deadline == "No deadline" }
-    ]
-
-    # case deadline
-    # when "Today"        then "badge_default"
-    # when "Expired"      then "badge_red"
-    # when "No deadline"  then "badge_neutral"
-    # else "badge_green"
-    # end
   end
 end
